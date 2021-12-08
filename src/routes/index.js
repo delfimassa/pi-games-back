4b0609190911d06f06ca79cea17ad7f13e6df02e
Delfi <delfimassa@gmail.com>
2021-12-07 23:39:49 -0300

    se cambio el search byName porque no andaban los de la api

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const axios = require("axios");
 const { Videogame, Genres, conn } = require("../db");
 const router = Router();
-const { APIKEY } = process.env;  
+const { APIKEY } = process.env;
 
 // GAMES DE LA API
 const getApiInfo = async () => {
@@ -26,7 +26,7 @@ const getApiInfo = async () => {
             name: elem.name,
           };
         }), //es un array con objetos x c/genero
-        createdInDb: false
+        createdInDb: false,
       });
     });
     i++;
@@ -40,7 +40,7 @@ const getDbInfo = async () => {
     include: {
       model: Genres,
       attributes: ["name"],
-      through: { attributes: [] }, 
+      through: { attributes: [] },
     },
   });
   const dbData = dbGames.map((e) => {
@@ -52,7 +52,7 @@ const getDbInfo = async () => {
       platforms: e.platforms,
       launching: e.launching,
       genres: e.Genres,
-      createdInDb: true
+      createdInDb: true,
     };
   });
   return dbData;
@@ -61,55 +61,26 @@ const getDbInfo = async () => {
 //TODOS LOS GAMES JUNTOS
 const getAllVideoGames = async () => {
   const dbInfo = await getDbInfo();
-  // const apiInfo = await getApiInfo();
-  // const allInfo = dbInfo.concat(apiInfo);
-  // return allInfo;
-  return dbInfo;
-};
-
-const getByName = async (name) => {
-  try {
-    let dataDB = await Videogame.findAll({
-      where: {
-        name: { [Op.iLike]: "%" + name + "%" },
-      },
-      include: Genres,
-    });
-    console.log("datadbd", dataDB);
-    let ordered = dataDB.map(e=>{
-      return {
-        id: e.id,
-        name: e.name,
-        image: e.image,
-        rating: e.rating,
-        platforms: e.platforms,
-        launching: e.launching,
-        genres: e.Genres,
-        createdInDb: true
-      };
-    } );
-     return ordered;
-  } catch (error) {
-  return "Videogame not found" ;
-  }
+  const apiInfo = await getApiInfo();
+  const allInfo = dbInfo.concat(apiInfo);
+  return allInfo;
 };
 
 //////////////////GET VIDEOGAMES AND VG BY NAME//////////////////
 router.get("/videogames", async (req, res) => {
   const { name } = req.query;
-  let AllVideogames = await getAllVideoGames();
-
+  let allGames = await getAllVideoGames(name);
   if (name) {
-    let videojuegoByQuery = await getByName(name);
-
-    videojuegoByQuery.length
-      ? res.status(200).send(videojuegoByQuery)
-      : res.status(404).send("Videogame not found");
+    let videogameName = await allGames.filter((e) =>
+      e.name.toLowerCase().includes(name.toLowerCase())
+    );
+    videogameName.length
+      ? res.status(200).send(videogameName)
+      : res.status(404).send("Lo sentimos, no encontramos ese videojuego");
   } else {
-    res.send(AllVideogames);
+    res.send(allGames);
   }
 });
-
 
 ////////////////GET GENRES/////////////////////
 router.get("/genres", async (req, res) => {
@@ -131,7 +102,6 @@ router.get("/genres", async (req, res) => {
   });
   const allGenres = await Genres.findAll();
   res.status(200).send(allGenres);
-
 });
 
 ////////////////GET PLATFORMS ///////////////////
@@ -144,8 +114,16 @@ router.get("/genres", async (req, res) => {
 ////////////////POST VIDEOGAME/////////////////////
 router.post("/videogame", async (req, res) => {
   try {
-    let { name, description, launching, rating, platforms, image, genres, createdInDb } =
-      req.body;
+    let {
+      name,
+      description,
+      launching,
+      rating,
+      platforms,
+      image,
+      genres,
+      createdInDb,
+    } = req.body;
     let gameCreated = await Videogame.create({
       name,
       description,
@@ -153,7 +131,7 @@ router.post("/videogame", async (req, res) => {
       rating,
       platforms,
       image,
-      createdInDb
+      createdInDb,
     });
     console.log("genres req.body", genres);
     await gameCreated.addGenres(genres);
@@ -165,27 +143,57 @@ router.post("/videogame", async (req, res) => {
 });
 
 ///////////////GET VIDEOGAME BY ID//////////////////
+async function getGameIdApi(id) {
+  try {
+    let game = await axios.get(
+      `https://api.rawg.io/api/games/${id}?key=1f02d81818664102a6fa63065e5be1ab`
+    );
+    return {
+      name: game.data.name,
+      id: game.data.id,
+      rating: game.data.rating,
+      description: game.data.description,
+      released: game.data.released,
+      genres: game.data.genres
+        ? game.data.genres
+        : "No disponemos del genero de este juego",
+      // .map(genre => genre.name) : "No disponemos del genero de este juego",
+      img: game.data.background_image,
+      platforms: game.data.platforms
+        ? game.data.platforms.map((plataforma) => plataforma.platform.name)
+        : "No disponemos de las plataformas de este juego",
+      stores: game.data.stores
+        ? game.data.stores.map((store) => store.store.name)
+        : "No disponemos los stores de este juego",
+    };
+  } catch (err) {
+    return [];
+  }
+}
+
 router.get("/videogames/:id", async (req, res) => {
   const id = req.params.id;
   try {
     if (id.length > 7) {
       let gamePk = await Videogame.findOne({
-        where: {id: id},
-        include:{
+        where: { id: id },
+        include: {
           model: Genres,
           attributes: ["name"],
-          through:{
-            attributes:[]
-          }
-        }
+          through: {
+            attributes: [],
+          },
+        },
       });
       if (gamePk) res.status(200).json(gamePk);
     } else {
-      let urlId = await axios.get(
-        `https://api.rawg.io/api/games/${id}?key=${APIKEY}`
-      );
-      let gameId = await urlId.data;
-      if (gameId) res.status(200).json(gameId);
+      let gameapi = await getGameIdApi(id);
+      if (gameapi) res.status(200).send(gameapi);
+      // let urlId = await axios.get(
+      //   `https://api.rawg.io/api/games/${id}?key=${APIKEY}`
+      // );
+      // let gameId = await urlId.data;
+      // if (gameId) res.status(200).json(gameId);
     }
   } catch (err) {
     res.status(404).send("GAME NOT FOUND");
